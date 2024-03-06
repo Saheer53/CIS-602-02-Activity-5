af01fb59c47e2e45298223223ac55807d2592bea
Saheer53 <123708064+Saheer53@users.noreply.github.com>
2024-03-06 00:18:56 +0000

    automated tests check

--- CalculateComission.js
+++ CalculateComission.js
@@ -28,7 +28,7 @@ function calculateCommissionFunction(lockQty, stockQty, barrelQty) {
         commissionRate = 0.2;
     }
 
-    // Calculate commission
+    // Calculate commission values
     let commission = totalSales * commissionRate;
 
     // Subtract commission for 1 lock
